3be0c479eb02a18291636bafbf4297cfc00b4d28
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2026-07-07 23:52:14 +0530

    otimized imports

--- src/main/java/com/ecommerce/project/repository/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.Category;
-import com.ecommerce.project.payload.CategoryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
