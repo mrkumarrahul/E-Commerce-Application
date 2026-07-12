5caf7aa5fb0722a11de3479d695920a103ceae4d
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2026-07-12 23:00:16 +0530

    Structure response into a proper structure from a string

---
+++ src/main/java/com/ecommerce/project/payload/APIResponse.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.payload;
+
+public class APIResponse {
+    public String message;
+    private boolean status;
+
+    public APIResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
